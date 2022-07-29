d544d04429ee923b7b4a66259d4765973e0a1fc5
Asif-Iftekher-Fahim27 <MN@DESKTOP-HD4LBED>
2022-07-30 01:07:59 +0600

    Add project files.

---
+++ Controllers/DBConnectionController.cs
@@ -0,0 +1,20 @@
+﻿using PathToJannah.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PathToJannah.Controllers
+{
+    public class DBConnectionController : Controller
+    {
+        // GET: DBConnection
+        PTJEntities db = new PTJEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PathToJannah.Models;
+
+namespace PathToJannah.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult AddorEdit(int id=0)
+        {
+            User userModel=new User();
+            return View(userModel);
+        }
+
+        [HttpPost]
+        public ActionResult AddorEdit(User userModel)
+        {
+            using ( PTJEntities dbModel = new PTJEntities())
+            {
+                if (dbModel.Users.Any(x=> x.Mobile == userModel.Mobile))
+                {
+                    ViewBag.MDuplicateMessage = "Mobile Number Already Exist";
+                    return View("AddorEdit",userModel);
+                }
+
+                if (dbModel.Users.Any(x => x.Email == userModel.Email))
+                {
+                    ViewBag.EDuplicateMessage = "Email is Already Exist";
+                    return View("AddorEdit", userModel);
+                }
+
+                dbModel.Users.Add(userModel);
+                dbModel.SaveChanges();
+
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration is Successful";
+            return View("AddorEdit", new User());
+        }
+    }
+}
