e3fe4f00644991dc2a5ed1a9aef6b13842da8dd0
iftekhar.fahim34 <iftekhar.fahim34@gmail.com>
2022-07-30 21:43:33 +0600

    upto choose

---
+++ Controllers/TopPageController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PathToJannah.Controllers
+{
+    public class TopPageController : Controller
+    {
+        // GET: TopPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+       
+    }
+}
