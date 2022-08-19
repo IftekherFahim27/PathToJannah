e49203e6eaed3091b1498b5c2aa8025ed1438186
Safwan <81897224+Supplemental-Farmer@users.noreply.github.com>
2022-08-19 12:30:10 +0600

    Dua_Hadith

---
+++ Controllers/HadithController.cs
@@ -0,0 +1,20 @@
+﻿using PathToJannah.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PathToJannah.Controllers
+{
+    public class HadithController : Controller
+    {
+        // GET: Hadith
+        PTJEntities database = new PTJEntities();
+        public ActionResult Index()
+        {
+            List<Hadith> hadith = database.Hadiths.ToList();
+            return View(hadith);
+        }
+    }
+}
