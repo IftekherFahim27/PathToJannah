a6252a6571e8004cca7077ab7a5386e8dad2f188
Safwan <81897224+Supplemental-Farmer@users.noreply.github.com>
2022-09-02 23:48:15 +0600

    donate

---
+++ Controllers/DonationController.cs
@@ -0,0 +1,18 @@
+﻿using PathToJannah.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PathToJannah.Controllers
+{
+    public class DonationController : Controller
+    {
+        // GET: Donation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
