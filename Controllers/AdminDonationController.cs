71a36dd66a456ead23fa07709f3023b3715294ef
Safwan <81897224+Supplemental-Farmer@users.noreply.github.com>
2022-09-03 11:56:23 +0600

    admin donation

---
+++ Controllers/AdminDonationController.cs
@@ -0,0 +1,20 @@
+﻿using PathToJannah.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PathToJannah.Controllers
+{
+    public class AdminDonationController : Controller
+    {
+        // GET: AdminDonation
+        PTJEntities db = new PTJEntities();
+        public ActionResult Index()
+        {
+            List<Donation> donation = db.Donations.ToList();
+            return View(donation);
+        }
+    }
+}
